89289a1d33b690f31e3736f265a7bbf9883ffdb5
rgm <git@rushd.xyz>
2025-11-17 14:28:18 +0000

    Take user defined number of inputs

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -7,19 +7,25 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scan1 = new Scanner(System.in);
-        System.out.println("Please enter customer ID: ");
-        String cusID = scan1.nextLine().trim();
+        System.out.println("Please enter number of entries: ");
+        int count = scan1.nextInt();
+        scan1.nextLine();
 
-        System.out.println("Please enter customer name: ");
-        String cusName = scan1.nextLine().trim();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Please enter customer ID: ");
+            String cusID = scan1.nextLine().trim();
 
-        System.out.println("Please enter customer email: ");
-        String cusEmail = scan1.nextLine().trim();
+            System.out.println("Please enter customer name: ");
+            String cusName = scan1.nextLine().trim();
 
-        System.out.println("Please enter customer phone number: ");
-        String cusPhone = scan1.nextLine().trim();
+            System.out.println("Please enter customer email: ");
+            String cusEmail = scan1.nextLine().trim();
 
-        Customer customer1 = new Customer(cusID, cusName, cusEmail, cusPhone);
-        System.out.println(customer1);
+            System.out.println("Please enter customer phone number: ");
+            String cusPhone = scan1.nextLine().trim();
+
+            Customer customer1 = new Customer(cusID, cusName, cusEmail, cusPhone);
+            System.out.println(customer1);
+        }
     }
 }
